ff0c493b9db65e8f80f5485221fad68470c3ab23
DILSHODBEK <dilshodsattar26@gmail.com>
2026-05-31 14:39:56 +0500

    Initial commit: setup main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+import json
+
+class helperDaveWorker:
+    def __init__(self):
+        self.buffer = []
+    def append_metric(self, val):
+        self.buffer.append(val)
+        return len(self.buffer)
+
+if __name__ == '__main__':
+    obj = helperDaveWorker()
+    print("Worker engine initialized.")
